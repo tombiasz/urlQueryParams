08f9d699180be0dded45c52e202aead60aa71d30
Tomasz Nowacki <tomasz.nowacki@netguru.co>
2019-01-19 19:23:21 +0100

    export Page via default export

--- src/__tests__/page.test.ts
+++ src/__tests__/page.test.ts
@@ -1,5 +1,4 @@
-import { Page } from '../page';
-
+import Page from '../page';
 
 describe('Page', () => {
   const attribute = 'fizz';
--- src/page.ts
+++ src/page.ts
@@ -1,4 +1,4 @@
-export class Page {
+export default class Page {
   groupName: string = 'page';
   attribute: string;
   value: string;
